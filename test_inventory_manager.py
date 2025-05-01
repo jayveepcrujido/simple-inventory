e600319133f755559de7ca2fb38d11eaeb421dd4
angelreinflatchica <angelreinlatchica@gmail.com>
2025-05-01 15:43:45 +0800

    update inventory_manager and test_inventory_manager

--- test_inventory_manager.py
+++ test_inventory_manager.py
@@ -12,6 +12,15 @@ class TestInventoryManager(unittest.TestCase):
     def test_view_empty_inventory(self):
         inventory = self.manager.view_inventory()
         self.assertEqual(inventory, {})
+    
+    def test_add_item(self):
+        self.manager.add_item("apples", 10)
+        self.assertEqual(self.manager.get_stock("apples"), 10)
+
+    def test_add_item_negative_quantity(self):
+        result = self.manager.add_item("orange", -3)
+        self.assertFalse(result)
+        self.assertNotIn("orange", data.inventory)
 
 
 if __name__ == "__main__":
